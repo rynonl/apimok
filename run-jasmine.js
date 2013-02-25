379a47f835fdb06c1fb6ac317c5e85c4e896acfa
Ryan O'Neill <rco8786@gmail.com>
2013-02-25 15:02:01 -0600

    xml output

--- run-jasmine.js
+++ run-jasmine.js
@@ -1,115 +1,237 @@
-/**
- * Wait until the test condition is true or a timeout occurs. Useful for waiting
- * on a server response or for a ui change (fadeIn, etc.) to occur.
- *
- * @param testFx javascript condition that evaluates to a boolean,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param onReady what to do when testFx condition is fulfilled,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
- */
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3001, //< Default Max Timeout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function() {
-            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-            } else {
-                if(!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    //console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
-                }
-            }
-        }, 100); //< repeat check every 100ms
-};
-
-if (phantom.args.length === 0 || phantom.args.length > 2) {
-    console.log('Usage: run-jasmine.js URL');
-    phantom.exit();
-}
-
-var page = typeof require!='undefined'? require('webpage').create() : typeof WebPage!='undefined' ? new WebPage() : null;
-
-if(page){
-    // Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
-    page.onConsoleMessage = function(msg) {
-        console.log(msg);
-    };
-
-    page.onResourceRequested = function (request) {
-        if(request.url.indexOf('.htm') != -1){
-            console.log('Requesting ' + request.url);
-        }
-    };
-
-    page.open(phantom.args[0], function(status){
-        if (status !== "success") {
-            console.log("Page not found: " + phantom.args[0]);
-            phantom.exit();
-        }
-        else {
-            waitFor(
-                function(){//waiting for this to return true
-                    return page.evaluate(function(){
-						var runner = document.body.querySelector('.runner');
-						if(!runner){
-							return !!runner;
-						}
-                        return !!runner.querySelector('.description');
-                    });
-                },
-                function(){
-                    page.evaluate( function() {
-						var suites = document.body.querySelectorAll('.suite');
-
-						for (var i = 0; i < suites.length; i++){
-							var suite = suites[i];
-
-							var suiteName = suite.querySelector('.description').innerText;
-							var passOrFail = suite.className.indexOf('passed') != -1 ? "Passed" : "Failed!";
-							console.log('Suite: '+suiteName+'\t'+passOrFail);
-							console.log('--------------------------------------------------------');
-							var specs = suite.querySelectorAll('.spec');
-							for (var j = 0; j < specs.length; j++){
-								var spec = specs[j];
-								var passed = spec.className.indexOf('passed') != -1;
-
-								var specName = spec.querySelector('.description').innerText;
-								var passOrFail = passed ? 'Passed' : "Failed!"
-								console.log('\t'+specName+'\t'+passOrFail);
-
-								if(!passed){
-									console.log('\t\t-> Message: '+spec.querySelector('.resultMessage.fail').innerText);
-									var trace = spec.querySelector('.stackTrace');
-									console.log('\t\t-> Stack: '+(trace!=null ? trace.innerText : 'not supported by phantomJS yet'));
-								}
-							}
-							console.log('');
-						}
-
-						var runner = document.body.querySelector('.runner');
-						console.log('--------------------------------------------------------');
-                        console.log('Finished: '+runner.querySelector('.description').innerText);
-                    });
-                    console.log('');
-                    phantom.exit();
-                },
-                300001
-            );
-        }
-    });
+// Verify arguments
+if (phantom.args.length === 0) {
+    console.log("Simple JasmineBDD test runner for phantom.js");
+    console.log("Usage: phantomjs-testrunner.js url_to_runner.html");
+    console.log("Accepts http:// and file:// urls");
+    console.log("");
+    console.log("NOTE: This script depends on jasmine.TrivialReporter being used\non the page, for the DOM elements it creates.\n");
+    phantom.exit(2);
 }
 else {
-    console.log('Could not create WebPage');
-    phantom.exit();
+    var args = phantom.args;
+    var pages = [], page, address, resultsKey, i, l;
+
+    var setupPageFn = function(p, k) {
+        return function() {
+            overloadPageEvaluate(p);
+            setupWriteFileFunction(p, k);
+        };
+    };
+
+    var xml_save_location = args[1];
+
+    address = args[0];
+    console.log("Loading " + address);
+
+    // if provided a url without a protocol, try to use file://
+    address = address.indexOf("://") === -1 ? "file://" + address : address;
+
+    // create a WebPage object to work with
+    page = require("webpage").create();
+    page.url = address;
+
+    // When initialized, inject the reporting functions before the page is loaded
+    // (and thus before it will try to utilize the functions)
+    resultsKey = "__jr" + Math.ceil(Math.random() * 1000000);
+    page.onInitialized = setupPageFn(page, resultsKey);
+    page.open(address, processPage(null, page, resultsKey));
+    pages.push(page);
+
+    page.onConsoleMessage = logAndWorkAroundDefaultLineBreaking;
+
+    // bail when all pages have been processed
+    setInterval(function(){
+        var exit_code = 0;
+        for (i = 0, l = pages.length; i < l; i++) {
+            page = pages[i];
+            if (page.__exit_code === null) {
+                // wait until later
+                return;
+            }
+            exit_code |= page.__exit_code;
+        }
+        phantom.exit(exit_code);
+    }, 100);
+}
+
+// Thanks to hoisting, these helpers are still available when needed above
+/**
+ * Logs a message. Does not add a line-break for single characters '.' and 'F' or lines ending in ' ...'
+ *
+ * @param msg
+ */
+function logAndWorkAroundDefaultLineBreaking(msg) {
+    var interpretAsWithoutNewline = /(^(\033\[\d+m)*[\.F](\033\[\d+m)*$)|( \.\.\.$)/;
+    if (navigator.userAgent.indexOf("Windows") < 0 && interpretAsWithoutNewline.test(msg)) {
+        var fs = require('fs');
+        // system.stdout.write(msg) ? wait for http://code.google.com/p/phantomjs/issues/detail?id=243 to be implemented
+        fs.write('/dev/stdout', msg, 'w');
+    } else {
+        console.log(msg);
+    }
+}
+
+/**
+ * Stringifies the function, replacing any %placeholders% with mapped values.
+ *
+ * @param {function} fn The function to replace occurrences within.
+ * @param {object} replacements Key => Value object of string replacements.
+ */
+function replaceFunctionPlaceholders(fn, replacements) {
+    if (replacements && typeof replacements === "object") {
+        fn = fn.toString();
+        for (var p in replacements) {
+            if (replacements.hasOwnProperty(p)) {
+                var match = new RegExp("%" + p + "%", "g");
+                do {
+                    fn = fn.replace(match, replacements[p]);
+                } while(fn.indexOf(match) !== -1);
+            }
+        }
+    }
+    return fn;
+}
+
+/**
+ * Replaces the "evaluate" method with one we can easily do substitution with.
+ *
+ * @param {phantomjs.WebPage} page The WebPage object to overload
+ */
+function overloadPageEvaluate(page) {
+    page._evaluate = page.evaluate;
+    page.evaluate = function(fn, replacements) { return page._evaluate(replaceFunctionPlaceholders(fn, replacements)); };
+    return page;
+}
+
+/** Stubs a fake writeFile function into the test runner.
+ *
+ * @param {phantomjs.WebPage} page The WebPage object to inject functions into.
+ * @param {string} key The name of the global object in which file data should
+ *                     be stored for later retrieval.
+ */
+// TODO: not bothering with error checking for now (closed environment)
+function setupWriteFileFunction(page, key) {
+    page.evaluate(function(){
+        window["%resultsObj%"] = {};
+        window.__phantom_writeFile = function(filename, text) {
+            window["%resultsObj%"][filename] = text;
+        };
+    }, {resultsObj: key});
+}
+
+/**
+ * Returns the loaded page's filename => output object.
+ *
+ * @param {phantomjs.WebPage} page The WebPage object to retrieve data from.
+ * @param {string} key The name of the global object to be returned. Should
+ *                     be the same key provided to setupWriteFileFunction.
+ */
+function getXmlResults(page, key) {
+    return page.evaluate(function(){
+        return window["%resultsObj%"] || {};
+    }, {resultsObj: key});
+}
+
+/**
+ * Processes a page.
+ *
+ * @param {string} status The status from opening the page via WebPage#open.
+ * @param {phantomjs.WebPage} page The WebPage to be processed.
+ */
+function processPage(status, page, resultsKey) {
+    if (status === null && page) {
+        page.__exit_code = null;
+        return function(stat){
+            processPage(stat, page, resultsKey);
+        };
+    }
+    if (status !== "success") {
+        console.error("Unable to load resource: " + address);
+        page.__exit_code = 2;
+    }
+    else {
+        var isFinished = function() {
+            return page.evaluate(function(){
+                // if there's a JUnitXmlReporter, return a boolean indicating if it is finished
+                if (jasmine.JUnitXmlReporter) {
+                    return jasmine.JUnitXmlReporter.finished_at !== null;
+                }
+                // otherwise, see if there is anything in a "finished-at" element
+                return document.getElementsByClassName("finished-at").length &&
+                       document.getElementsByClassName("finished-at")[0].innerHTML.length > 0;
+            });
+        };        
+        var getResults = function() {
+            return page.evaluate(function(){
+                var writeToConsole = function() {
+                    var suites = document.body.querySelectorAll('.suite');
+
+                    for (var i = 0; i < suites.length; i++){
+                        var suite = suites[i];
+
+                        var suiteName = suite.querySelector('.description').innerText;
+                        var passOrFail = suite.className.indexOf('passed') != -1 ? "Passed" : "Failed!";
+                        console.log('Suite: '+suiteName+'\t'+passOrFail);
+                        console.log('--------------------------------------------------------');
+                        var specs = suite.querySelectorAll('.spec');
+                        for (var j = 0; j < specs.length; j++){
+                            var spec = specs[j];
+                            var passed = spec.className.indexOf('passed') != -1;
+
+                            var specName = spec.querySelector('.description').innerText;
+                            var passOrFail = passed ? 'Passed' : "Failed!"
+                            console.log('\t'+specName+'\t'+passOrFail);
+
+                            if(!passed){
+                                console.log('\t\t-> Message: '+spec.querySelector('.resultMessage.fail').innerText);
+                                var trace = spec.querySelector('.stackTrace');
+                                console.log('\t\t-> Stack: '+(trace!=null ? trace.innerText : 'not supported by phantomJS yet'));
+                            }
+                        }
+                        console.log('');
+                    }
+
+                    var runner = document.body.querySelector('.runner');
+                    console.log('--------------------------------------------------------');
+                    console.log('Finished: '+runner.querySelector('.description').innerText);
+                
+                };
+
+                writeToConsole();
+
+                return document.getElementsByClassName("description").length &&
+                       document.getElementsByClassName("description")[0].innerHTML.match(/(\d+) spec.* (\d+) failure.*/) ||
+                       ["Unable to determine success or failure."];
+            });
+        };
+        var ival = setInterval(function(){
+            if (isFinished()) {
+
+                // get the results that need to be written to disk
+                var fs = require("fs"),
+                    xml_results = getXmlResults(page, resultsKey),
+                    output,
+                    agg_xml;
+                for (var filename in xml_results) {
+                    if (xml_results.hasOwnProperty(filename) && (output = xml_results[filename]) && typeof(output) === "string") {
+                        agg_xml += output;
+                        fs.write(xml_save_location + "/" + filename, output, "w");
+                    }
+                }
+                    fs.write(xml_save_location + "/TEST-Results.xml", agg_xml, "w");
+
+                // print out a success / failure message of the results
+                var results = getResults();
+                var failures = Number(results[2]);
+                if (failures > 0) {
+                    page.__exit_code = 1;
+                    clearInterval(ival);
+                }
+                else {
+                    page.__exit_code = 0;
+                    clearInterval(ival);
+                }
+            }
+        }, 100);
+    }
 }
